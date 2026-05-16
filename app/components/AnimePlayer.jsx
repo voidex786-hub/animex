de9af20e76218aac1c521e40d3dea423279a372f
voidex786 <voidex786@gmail.com>
2026-05-16 20:51:45 +0530

    removed vidstack build issue

--- app/components/AnimePlayer.jsx
+++ app/components/AnimePlayer.jsx
@@ -1,17 +1,12 @@
 "use client"
 
-import { MediaPlayer, MediaProvider } from "@vidstack/react"
-
 export default function AnimePlayer({ src }) {
   return (
-    <MediaPlayer
-      title="Animex Player"
+    <video
       src={src}
-      autoPlay
       controls
-      className="w-full aspect-video rounded-3xl overflow-hidden border border-white/10 bg-black"
-    >
-      <MediaProvider />
-    </MediaPlayer>
+      autoPlay
+      className="w-full rounded-3xl border border-white/10 bg-black"
+    />
   )
 }
